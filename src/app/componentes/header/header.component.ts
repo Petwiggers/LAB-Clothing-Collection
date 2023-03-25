9c549fda32b45d03027f174823f87d4eeabf471b
Peterson Wiggers <petersonwiggersoficial@gmail.com>
2023-03-25 16:46:00 -0300

    Header Terminado

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+ 
+  constructor(private rota: Router){}
+  sair(){
+    console.log('Executou Sair!');
+    
+    localStorage.clear();
+    this.rota.navigate(['/Login'])
+  }
+
 
 }
